440465390299eebfca3c22957fc1eb3245172982
Gokmen Gorgen <gokmen@alageek.com>
2014-12-21 23:45:18 +0200

    truncate device id.

--- django_parse_push/admin.py
+++ django_parse_push/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
+from django.utils.text import Truncator
 from django_parse_push.models import Device
 
 
 class DeviceAdmin(admin.ModelAdmin):
-    list_display = ("device_id", "device_type", "user", "is_active", "created_at")
+    list_display = ("truncated_device_id", "device_type", "user", "is_active", "created_at")
+
+    def truncated_device_id(self, obj):
+        return Truncator(obj.device_id).chars(50)
 
 
 admin.site.register(Device, DeviceAdmin)
